c6abace1ff9860ce0c80094e400df7b09d50e230
Constantin Kalversberg <constantin.kalversberg@alumni.fh-aachen.de>
2019-11-26 23:20:43 +0100

    P3 abgabe

--- src/P3/BenutzerVerwaltungAdminTest.java
+++ src/P3/BenutzerVerwaltungAdminTest.java
@@ -4,20 +4,29 @@ import P2.Benutzer;
 import P2.BenutzerExits;
 import P2.BenutzerNotInList;
 import P2.BenutzerVerwaltungAdmin;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BenutzerVerwaltungAdminTest {
+    BenutzerVerwaltungAdmin admin;
+    Benutzer user, user2;
+    @Before
+    public void setUp() {
+        admin = new BenutzerVerwaltungAdmin();
+        admin.dbInitialisieren();
+
+        user = new Benutzer("peter", "123".toCharArray());
+        user2= new Benutzer("peter1", "123".toCharArray());
+    }
+
 
     /**
      * Check ob der Benutzer richtig eingetragen wird. Hierzu Trage die nutzer zweimal ein die Methode fail() beendet den Test ohne Meldung dazu habe ich e.getMessage()
      */
     @Test
     public void benutzerEintragen() {
-        BenutzerVerwaltungAdmin admin = new BenutzerVerwaltungAdmin();
-        admin.dbInitialisieren();
-        Benutzer user = new Benutzer("peter", "123".toCharArray());
-        Benutzer user2= new Benutzer("peter1", "123".toCharArray());
         try {
             admin.benutzerEintragen(user);
         } catch (BenutzerExits e) {
@@ -50,18 +59,15 @@ public class BenutzerVerwaltungAdminTest {
      */
     @Test
     public void benutzerOk() {
-        BenutzerVerwaltungAdmin admin = new BenutzerVerwaltungAdmin();
-        admin.dbInitialisieren();
-        admin.deleteAll();
-        assertFalse(admin.benutzerOk(new Benutzer("peter","123".toCharArray())));
+        assertFalse(admin.benutzerOk(user));
         try {
-            admin.benutzerEintragen(new Benutzer("peter","123".toCharArray()));
+            admin.benutzerEintragen(user);
         } catch (BenutzerExits benutzerExits) {
             benutzerExits.printStackTrace();
             fail();
         }
-        assertTrue(admin.benutzerOk(new Benutzer("peter","123".toCharArray())));
-        assertFalse(admin.benutzerOk(new Benutzer("peter1","123".toCharArray())));
+        assertTrue(admin.benutzerOk(user));
+        assertFalse(admin.benutzerOk(user2));
     }
 
 
@@ -70,25 +76,36 @@ public class BenutzerVerwaltungAdminTest {
      */
     @Test
     public void benutzerLoeschen()  {
-        BenutzerVerwaltungAdmin admin = new BenutzerVerwaltungAdmin();
-        admin.dbInitialisieren();
-        admin.deleteAll();
-        Benutzer peter = new Benutzer("peter", "123".toCharArray());
         try {
-            admin.benutzerLoeschen(peter);
+            admin.benutzerLoeschen(user);
             fail("Muss BenutzerNotInList werfen");
         } catch (BenutzerNotInList e) {
             e.printStackTrace();
         }
         try {
-            admin.benutzerEintragen(peter);
+            admin.benutzerEintragen(user);
         } catch (BenutzerExits benutzerExits) {
             benutzerExits.printStackTrace();
         }
         try {
-            admin.benutzerLoeschen(peter);
+            admin.benutzerLoeschen(user);
         } catch (BenutzerNotInList benutzerNotInList) {
             benutzerNotInList.printStackTrace();
+            fail();
         }
+    }
+
+    @Test
+    public void serialisieren()  {
+        try {
+            admin.benutzerEintragen(user);
+            admin.benutzerEintragen(user2);
+        } catch (BenutzerExits benutzerExits) {
+            benutzerExits.printStackTrace();
+            fail();
+        }
+        BenutzerVerwaltungAdmin admin2 = new BenutzerVerwaltungAdmin();
+        assertTrue(admin2.benutzerOk(user));
+        assertTrue(admin2.benutzerOk(user2));
     }
 }
